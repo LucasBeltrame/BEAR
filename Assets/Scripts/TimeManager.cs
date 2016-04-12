922086520ae9149398581baec856a0f7671f8334
Lucas Beltrame <lucas.beltrame@he-arc.ch>
2016-04-12 14:57:48 +0200

    Ajout de la gestion du temps et début de l'UI

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,17 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-    public float nbMinutesPerDay = 3.0f;
+    public float nbMinutesPerDay = 5.0f;
+    public Text dayText;
+    public Text timeText;
+    public Text AnnonceJourText;
+    public Canvas annonceCanvas;
+
+    private bool countTime = false;
     private float timeElapsed = 0.0f;
     private const float SEC_PER_MIN = 60.0f;
     private int noDay;
-	// Use this for initialization
-	void Start ()
+    private float actualTime = 12.0f;
+    private float hoursPassed = 0.0f;
+    private  CanvasGroup annonceCanvasGroup;
+    // Use this for initialization
+    void Start ()
 	{
 	    timeElapsed = 0.0f;
 	    noDay = 1;
+        annonceCanvasGroup = annonceCanvas.GetComponent<CanvasGroup>();
 
 	}
 	
@@ -25,16 +37,47 @@ public class TimeManager : MonoBehaviour
     {
         if (timeElapsed == 0.0f)
         {
-            print("Day " + noDay + " begin ! ");
+            dayText.text = "Jour : " + noDay.ToString();
+            AnnonceJourText.text = "Jour " + noDay.ToString();
         }
 
-        timeElapsed += Time.deltaTime;
+        if (countTime == true)
+        {
+            timeElapsed += Time.deltaTime;
+        }
+        else
+        {
+            annonceCanvasGroup.alpha -= 0.002f;
+            if (annonceCanvasGroup.alpha <= 0)
+            {
+                countTime = true;
+                annonceCanvasGroup.alpha = 0.0f;
+            }
+        }
 
         if (timeElapsed >= nbMinutesPerDay * SEC_PER_MIN)
         {
-            print("Day " + noDay + " Ended ! ");
             noDay++;
             timeElapsed = 0.0f;
+            hoursPassed = 0.0f;
+            countTime = false;
+            annonceCanvasGroup.alpha = 1.0f;
         }
+
+        timeText.text = GetActualTimeString();
+    }
+
+    string GetActualTimeString()
+    {
+        actualTime = 12.0f;
+        float displayTime = 12.0f;
+
+        float increment = timeElapsed / (nbMinutesPerDay * SEC_PER_MIN);
+        actualTime += increment;
+
+        TimeSpan timeSpan = TimeSpan.FromHours(actualTime * nbMinutesPerDay);
+        string resultText = string.Format("{0:D2}:{1:D2}", timeSpan.Hours, timeSpan.Minutes);
+
+        return resultText;
     }
 }
