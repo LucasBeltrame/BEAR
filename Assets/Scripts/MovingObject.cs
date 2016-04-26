7dbc537616179b46d93484993837f91ea8c8be78
Alex <alexandre.straubhaar@he-arc.ch>
2016-04-26 12:26:34 +0200

    Pleins de choses oui oui

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObject : MonoBehaviour
+{
+	public float moveTime = 0.5f;
+	public LayerMask blockingLayer;
+
+	private BoxCollider2D boxCollider;
+	private Rigidbody2D rb2D;
+	private float inverseMoveTime;
+
+
+	// Use this for initialization
+	protected virtual void Start ()
+	{
+		boxCollider = GetComponent<BoxCollider2D> ();
+		rb2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
